92c40f804c4223ae1b22af3ed24f1ef95f416bfe
Paulo Hercilio Araujo Santana <phasantana@sedurbs.se>
2023-06-12 09:37:25 -0300

    fix: found what were the bug related to the icons

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -3,65 +3,76 @@ import { Warning, Check, Info } from "@phosphor-icons/react";
 import { Type } from "../types/Type";
 
 interface NotificationProps {
-    message: string;
-    type: Type
+  message: string;
+  type: Type;
 }
 
 const ContainerStyle: React.CSSProperties = {
-    position: 'fixed',
-    top: '24px',
-    right: '24px',
-    padding: '8px',
-    width: '200px',
-    zIndex: 9999999999,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'start',
-    gap: '8x',
-}
+  position: "fixed",
+  top: "24px",
+  right: "24px",
+  padding: "8px",
+  width: "200px",
+  zIndex: 9999999999,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "start",
+  gap: "8x",
+  borderRadius: "4px",
+};
 
 const IconStyle: React.CSSProperties = {
-    padding: '4px',
-    color: '#fff',
-}
+  padding: "4px",
+  color: "#fff",
+};
 
-const Icon = ({ type }: { type: Type }) => {
-    switch (type) {
-        case 'success':
-            return <Check size={32} />
-        case 'info':
-            return <Info size={32} />
-        case 'error':
-            return <Warning size={32} />
-        case 'warning':
-            return <Warning size={32} />
-        default:
-            return <Check size={32} />
-    }
-}
+
+const backgroundColor = {
+  success: {
+    full: "rgba(76, 175, 80, 1)",
+    light: "rgba(76, 175, 80, 0.2)",
+  },
+  info: {
+    full: "rgba(33, 150, 243, 1)",
+    light: "rgba(33, 150, 243, 0.2)",
+  },
+  error: {
+    full: "rgba(244, 67, 54, 1)",
+    light: "rgba(244, 67, 54, 0.2)",
+  },
+  warning: {
+    full: "rgba(255, 152, 0, 1)",
+    light: "rgba(255, 152, 0, 0.2)",
+  }
+};
+
+const Icon = {
+  success: <Check size={32} />,
+  info: <Info size={32} />,
+  error: <Warning size={32} />,
+  warning: <Warning size={32} />,
+};
 
 export const Notification = ({ message, type }: NotificationProps) => {
-
-    const backgroundColor = {
-        success: '#4CAF50',
-        info: '#2196F3',
-        error: '#f44336',
-        warning: '#ff9800'
-    }
-
-    return (
-        <div style={ContainerStyle}>
-            <div style={{
-                ...IconStyle,
-                backgroundColor: backgroundColor[type],
-            }}>
-                <Icon type={type} />
-            </div>
-            {message}
-        </div>
-    )
-}
+  return (
+    <div style={{
+      ...ContainerStyle,
+      backgroundColor: backgroundColor[type].light,
+    }}>
+      <div
+        style={{
+          ...IconStyle,
+          backgroundColor: backgroundColor[type].full,
+        }}
+      >
+        {/* {Icon[type] ?? <Check size={32} />} */}
+        {<Check size={32} />}
+      </div>
+      {message}
+    </div>
+  );
+};
 
 export const Any = () => {
-    <h1>Test</h1>
-}
+  return <h1>Test</h1>;
+};
